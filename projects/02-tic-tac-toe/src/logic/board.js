d57f4ab0d7a35ab9a6acbd1ad924a9e16692ca3f
miguellitta <miguel.litta@gmail.com>
2026-01-29 10:37:38 -0300

    changes movie-search

---
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,25 @@
+import { WINNER_COMBOS } from "../constants"
+
+export const checkWinnerFrom = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    // para ver si x u o ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && // 0 -> x u o
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a] // x u o
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  export const checkEndGame = (newBoard) => {
+    // revisamos si hay un empate
+    // si no hay mas espacios vacíos
+    // en el tablero
+    return newBoard.every((square) => square !== null)
+  }
